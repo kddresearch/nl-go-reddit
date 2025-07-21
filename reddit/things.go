7126e412fdfdfc52138a752545287845519dd47d
Wesley Baldwin <74224353+Legonois@users.noreply.github.com>
2025-07-21 20:19:31 +0000

    added comment mediametadata

--- reddit/things.go
+++ reddit/things.go
@@ -435,6 +435,8 @@ type Comment struct {
 	NSFW        bool `json:"over_18"`
 
 	Replies Replies `json:"replies"`
+
+	MediaMetadata map[string]MediaMetadata `json:"media_metadata,omitempty"`
 }
 
 // HasMore determines whether the comment has more replies to load in its reply tree.
